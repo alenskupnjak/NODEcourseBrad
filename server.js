08649b709914967f4c9b16a04b8c34994f860e97
AlenS <alenskupnjak@yahoo.com>
2020-07-02 17:44:56 +0200

    002 CRUD server

--- server.js
+++ server.js
@@ -8,9 +8,48 @@ dotenv.config({ path: './config/config.env' });
 const app = express();
 
 // definiranje porta
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000;
 
+// dohvati sve
+app.get('/api/v1/bootcamps', (req, res) => {
+  res.status(400).json({
+    sucess: true,
+    msg: 'Prikaži sve bootcamps',
+  });
+});
 
+// dohvati jednog
+app.get('/api/v1/bootcamps/:id', (req, res) => {
+  res.status(400).json({
+    sucess: true,
+    msg: `Prikaži ${req.params.id}`,
+  });
+});
+
+app.post('/api/v1/bootcamps', (req, res) => {
+  res.status(200).json({
+    sucess: true,
+    msg: 'Kreiraj bootcamps',
+  });
+});
+
+// UPDATE
+app.put('/api/v1/bootcamps/:id', (req, res) => {
+  res.status(200).json({
+    sucess: true,
+    msg: `Prikaži ${req.params.id}`,
+  });
+});
+
+// DELETE
+app.delete('/api/v1/bootcamps/:id', (req, res) => {
+  res.status(200).json({
+    sucess: true,
+    msg: `Obriši ${req.params.id}`,
+  });
+});
+
+// prati zahtijeve koji stizu
 app.listen(PORT, () => {
   console.log(`App listening on port ${process.env.PORT}`);
 });
