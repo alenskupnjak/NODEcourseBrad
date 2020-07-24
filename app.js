cdf6fb0dd952c91bd6287b0c5a0ac581e28ac105
AlenS <alenskupnjak@yahoo.com>
2020-07-24 13:05:40 +0200

    054a refresh delete page

--- app.js
+++ app.js
@@ -74,7 +74,7 @@ app.use(xss());
 // Rate limiting
 const limiter = rateLimit({
   windowMs: 10 * 60 * 1000, // 10 minutes
-  max: 100, // limit each IP to 100 requests per windowMs
+  max: 1000, // limit each IP to 100 requests per windowMs
 });
 
 //  apply to all requests
